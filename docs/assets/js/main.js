0979c5644615a2c6b66a53a5cc1f2dd7b32447af
Tiffany Scott <Tiffany.Scott@icf.com>
2020-10-14 23:18:13 -0400

    New header updates for PL header nav and desktop main example  nav. More to come.

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -2,4 +2,17 @@
 jQuery(document).ready(function($) {
     //$("h2").css("color", "red");
 	//alert("JQUERY IS HERE");
-});
+
+    //Desktop Main Menu Hover Support
+    $(".mainNav li").children("ul").hide();
+    $(".mainNav li").hover(function() {
+        var isHovered = $(this).is(":hover");
+        if (isHovered) {
+            $(this).children("ul").show(300);
+            $(this).children("ul li .third-level").hide();
+        } else {
+            $(this).children("ul").stop().hide(300);
+        }
+    });
+
+});// End document load function
